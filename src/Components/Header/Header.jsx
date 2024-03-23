fdde078b580c1c0abd164929a4baf4c6e41bc84a
George <g.m.6@live.com>
2024-03-23 16:00:44 +0200

    initial

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+
+
+
+const Header = () => {
+    return (
+        <header>
+            <h1>React App</h1>
+
+
+            <div className="header-left">
+                <img src="" alt="" />
+                <div className="header-search">
+                    <input type="text" placeholder="Search" />
+                    <i className="fas fa-search"></i>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
