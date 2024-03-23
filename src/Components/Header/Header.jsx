6360400660848f2c4aba8d1b956ab1ca61324ace
George <g.m.6@live.com>
2024-03-23 16:27:09 +0200

    initial

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,7 @@
-
-
+import React from 'react';  
+import SearchIcon from '@mui/icons-material/Search';
+import './style.css';
+import '/client/src/styles/utilities.css'
 
 const Header = () => {
     return (
@@ -8,8 +10,9 @@ const Header = () => {
 
 
             <div className="header-left">
-                <img src="" alt="" />
+                <img src="/client/assets/logo.png" alt="logo" />
                 <div className="header-search">
+                    <SearchIcon />
                     <input type="text" placeholder="Search" />
                     <i className="fas fa-search"></i>
                 </div>
